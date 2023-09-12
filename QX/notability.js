ea9df48e6006f281b9ba87e109f32726c8c64198
zifeiyu <111054005+Zifeiyu-0@users.noreply.github.com>
2023-09-12 12:59:04 +0800

    Add files via upload

---
+++ QX/notability.js
@@ -0,0 +1,45 @@
+/*************************************
+
+项目名称：Notability
+脚本作者：chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/notability\.com\/(global|subscriptions) url script-response-body https://raw.githubusercontent.com/Zifeiyu-0/Script/main/QX/notability.js
+
+[mitm]
+hostname = notability.com
+
+*************************************/
+
+
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023 = {
+   "data" : {
+     "processAppleReceipt" : {
+       "error" : 0,
+       "subscription" : {
+         "productId" : "com.gingerlabs.Notability.premium_subscription",
+         "originalTransactionId" : "570001184068302",
+         "tier" : "premium",
+         "refundedDate" : null,
+         "refundedReason" : null,
+         "isInBillingRetryPeriod" : false,
+         "expirationDate" : "2099-09-09T09:09:09.000Z",
+         "gracePeriodExpiresAt" : null,
+         "overDeviceLimit" : false,
+         "expirationIntent" : null,
+         "__typename" : "AppStoreSubscription",
+         "user" : null,
+         "status" : "canceled",
+         "originalPurchaseDate" : "2022-09-09T09:09:09.000Z"
+       },
+       "__typename" : "SubscriptionResult"
+    }
+  }
+};
+
+$done({body : JSON.stringify(chxm1023)});
